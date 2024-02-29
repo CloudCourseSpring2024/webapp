c07b8f77acf5ed0c7e22099f7d710b16cb8a8ad3
HasiniMuvva <muvva.h@northeastern.edu>
2024-02-29 00:40:20 -0500

    5th one commit

--- nodeinstallement.sh
+++ nodeinstallement.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# it installs Node.js on CentOS
+# This script installs Node.js on CentOS
 
+
+# Install Node.js
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
 
 
 sudo yum install -y nodejs
 
-# Verify if it is installed or not
+# Verify installation
 node --version
 npm --version
